90d879154c2529ba7fa2804d06f0f9798332c01f
Lucie Zýmová <zyminka@gmail.com>
2020-06-20 13:41:57 +0200

    pocasi

--- projekt/src/components/App.vue
+++ projekt/src/components/App.vue
@@ -4,11 +4,22 @@
       <img class="logo" src="../assets/rsz_cover.png" />
       <nav class="navigace">
         <router-link class="link" to="/homepage">Mapa</router-link>
-        <router-link class="link" to="/homepage">Seznam tras</router-link>
-        <router-link class="link" to="/trasa/:id">Go to Page 1</router-link>
+        <router-link class="link" to="/trasa/:id">Seznam tras</router-link>
       </nav>
+      <a
+        class="weatherwidget-io"
+        href="https://forecast7.com/cs/50d0814d44/prague/"
+        data-label_1="Praha"
+        data-label_2="Počasí"
+        data-icons="Climacons Animated"
+        data-days="3"
+        data-highcolor="#783192"
+        data-lowcolor="#59BCC8"
+        data-cloudfill="#616a6b"
+      >Praha Počasí</a>
     </div>
-    <router-view class="view" />
+    <router-view />
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -16,6 +27,16 @@
 export default {
   name: "App"
 };
+!(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://weatherwidget.io/js/widget.min.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+})(document, "script", "weatherwidget-io-js");
 </script>
 
 <style lang="css">
@@ -42,14 +63,16 @@ html {
 .link {
   color: #616a6b;
   text-decoration: none;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
 .navigace {
   align-self: center;
   margin-right: 50px;
 }
-
-.view {
+.weatherwidget-io {
+  width: 450px;
 }
 </style>
 
--- projekt/src/components/Homepage.vue
+++ projekt/src/components/Homepage.vue
@@ -8,11 +8,17 @@
       <button class="hpbut">Button</button>
     </div>
     <div class="mapa">
-      <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=1EgEwwP7VEbi22zyufEuLC7sFEWcsrhOR"
-        width="640"
-        height="480"
-      ></iframe>
+      <div class="main">
+        <h2>Toto je hlavní mapa</h2>
+        <iframe
+          src="https://www.google.com/maps/d/u/0/embed?mid=1EgEwwP7VEbi22zyufEuLC7sFEWcsrhOR"
+          width="640"
+          height="480"
+        ></iframe>
+      </div>
+    </div>
+    <div class="seznam">
+      <h2>Toto je část se seznamem</h2>
     </div>
   </div>
 </template>
@@ -35,27 +41,44 @@ export default {};
   justify-content: flex-end;
   padding: 50px;
 }
-.hpbut {
+button {
   width: 200px;
   color: rgba(0, 0, 0, 1);
   font-size: 15px;
-  height: 50px;
   border-radius: 10px;
   border: none;
   align-self: center;
+  padding: 15px 0;
 }
 h1 {
   color: white;
   font-size: 60px;
-
+  align-self: center;
+  text-align: center;
+}
+h2 {
+  align-self: center;
   text-align: center;
 }
 .about {
   color: white;
-  padding-bottom: 20px;
+  margin-bottom: 100px;
+  width: 350px;
+  align-self: center;
+  text-align: center;
 }
 
 .mapa {
   display: flex;
+  flex-direction: column;
+}
+
+.main {
+  align-self: center;
+}
+
+.seznam {
+  display: flex;
+  flex-direction: column;
 }
 </style>
