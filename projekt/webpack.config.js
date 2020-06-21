a573b738883d4b31f3b9b64013550814516b8c01
Lucie Zýmová <zyminka@gmail.com>
2020-06-21 14:38:11 +0200

    instal google fonts

--- projekt/webpack.config.js
+++ projekt/webpack.config.js
@@ -3,61 +3,75 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 const path = require('path');
 
+const GoogleFontsPlugin = require("google-fonts-webpack-plugin")
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.vue'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name]-[hash:6].[ext]',
-              outputPath: 'assets/img',
-              esModule: false,
-            }
-          }
-        ]
-      },
-      {
-        test: /\.vue$/,
-        use: 'vue-loader',
-      },
-    ],
-  },
+  "entry": "index.js",
+  /* ... */
   plugins: [
-    new VueLoaderPlugin(),
-    new CopyPlugin({
-      patterns: [{ 
-        from: 'src/assets', 
-        to: 'assets',
-        noErrorOnMissing: true, 
-      }],
-    }),
-  ],
-};
+    new GoogleFontsPlugin({
+      fonts: [
+        { family: "Source Sans Pro" },
+        { family: "Roboto", variants: ["100", "300", "400", "500", "700"] }
+      ]
+    })
+  ]
+
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.vue'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['vue-style-loader', 'css-loader']
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name]-[hash:6].[ext]',
+                outputPath: 'assets/img',
+                esModule: false,
+              }
+            }
+          ]
+        },
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new CopyPlugin({
+        patterns: [{
+          from: 'src/assets',
+          to: 'assets',
+          noErrorOnMissing: true,
+        }],
+      }),
+    ],
+  };
